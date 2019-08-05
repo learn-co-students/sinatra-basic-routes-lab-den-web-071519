a1167872e9eec21d9474dcdf60380507f2414d24
Ethan Collins <ethantcollins98@gmail.com>
2019-08-05 14:33:13 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Ethan"
+    end
+
+    get '/hometown' do
+        "My hometown is Houston"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Piece of Your Heart by Meduza"
+    end
 end
